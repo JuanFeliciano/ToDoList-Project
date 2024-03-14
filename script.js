0f041dd28eaadc94295490ad735de5e8cb2f0fea
Estagiario 2 <estagiario2@movtech.com.br>
2024-03-14 16:31:51 -0300

    feat: finish commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 function addTask() {
   if (inputBox.value === "") {
-    alert("You must write something!");
+    alert("Você precisa escrever algo!");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
